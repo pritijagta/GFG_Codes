c21ac64a220d37bd104a57aa6292193202131711
pritijagtap <130908773+pritijagta@users.noreply.github.com>
2024-12-25 10:20:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Set Matrix Zeroes/set-matrix-zeroes.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int arr[][] = new int[n][m];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            new Solution().setMatrixZeroes(arr);
+            for (int[] row : arr) {
+                for (int val : row) {
+                    System.out.print(val + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//Back-end complete function Template for Java
+class Solution {
+    public void setMatrixZeroes(int[][] mat) {
+        
+        int row[]=new int[mat.length];
+        int col[]=new int[mat[0].length];
+        
+        for(int i=0;i<mat.length;i++)
+        {
+            for(int j=0;j<mat[0].length;j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+      
+      for(int i=0;i<row.length;i++)
+      {
+          for(int j=0;j<col.length;j++)
+          {
+          if(row[i]==1 || col[j]==1)
+          {
+              mat[i][j]=0;
+              
+          }
+          }
+      }
+        
+    }
+}
